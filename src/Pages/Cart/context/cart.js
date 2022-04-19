3ea705b6a537f35dd56843a4a371ad2e71b695dd
Dayana <dayanamrd@gmail.com>
2022-04-19 17:17:00 -0300

    subindo carrinho

---
+++ src/Pages/Cart/context/cart.js
@@ -0,0 +1,50 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export default function CartProvider({ children }) {
+  const [productsCart, setProductsCart] = useState([]);
+
+  function addProducToCart(id) {
+    const copyProductsCart = [...productsCart];
+
+    const item = copyProductsCart.filter((product) => product.id === id);
+
+    if (item.length > 0) {
+      item[0].quantity = item[0].quantity + 1;
+    } else {
+      copyProductsCart.push({ id: id, quantity: 1});
+    }
+    console.log(copyProductsCart);
+    
+    setProductsCart(copyProductsCart);
+  }
+
+  function removeProductToCart(id) {
+    const copyProductsCart = [...productsCart];
+
+    const item = copyProductsCart.filter((product) => product.id === id);
+
+    if (item.length > 0) {
+      if (item[0].quantity > 1) {
+        item[0].quantity = item[0].quantity - 1;
+        setProductsCart(copyProductsCart);
+      } else {
+        const arrayFilterd = copyProductsCart.filter(
+          (product) => product.id !== id
+        );
+        setProductsCart(arrayFilterd);
+      }
+    } else {
+      alert("Esse produto não existe no carrinho");
+    }
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ productsCart, addProducToCart, removeProductToCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
