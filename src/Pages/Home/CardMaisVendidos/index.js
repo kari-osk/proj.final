f3c78df45f118fc2571ff774aa261cf1f4dbd4b9
kari-osk <karinaosuka@gmail.com>
2022-04-20 21:19:12 -0300

    alterei o padding das imagens

--- src/Pages/Home/CardMaisVendidos/index.js
+++ src/Pages/Home/CardMaisVendidos/index.js
@@ -1,25 +1,27 @@
-import { Card, Button } from "react-bootstrap";
-import "./style.css";
-import { useEffect, useState } from "react";
-import { getAllProducts } from '../../../Service/Api';
-import { Link } from "react-router-dom";
-
+import { Card, Button } from 'react-bootstrap'
+import './style.css'
+import { useEffect, useState } from 'react'
+import { getAllProducts } from '../../../Service/Api'
+import { Link } from 'react-router-dom'
 
 export default function CardMaisVendidos() {
-
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
-    getAllProducts().then(data => setProducts(data.content));
-  }, []);
+    getAllProducts().then(data => setProducts(data.content))
+  }, [])
 
   return (
     <div className="group_card container">
       {/* slice limita o número de produtos que serão exibidos */}
-      {products.slice(4, 7).map((product) => (
+      {products.slice(4, 7).map(product => (
         <Card className="box_card" key={product.id}>
           <div className="box_imageMaisvend">
-            <Card.Img className="image_maisvend" variant="top" src={product.image} />
+            <Card.Img
+              className="image_maisvend"
+              variant="top"
+              src={product.image}
+            />
           </div>
           <Card.Body>
             <Card.Title>{product.title}</Card.Title>
@@ -27,11 +29,13 @@ export default function CardMaisVendidos() {
           </Card.Body>
           <Card.Footer className="d-grid gap-2">
             <Link to={`/details/${product.id}`}>
-              <Button className="link-buy" variant="outline-dark" size="lg">Comprar por R$ {product.price}</Button>
+              <Button className="link-buy" variant="outline-dark" size="lg">
+                Comprar por R$ {product.price}
+              </Button>
             </Link>
           </Card.Footer>
         </Card>
       ))}
     </div>
-  );
+  )
 }
--- src/Pages/Home/CardNovos/index.js
+++ src/Pages/Home/CardNovos/index.js
@@ -1,27 +1,31 @@
-import { Card, Button } from "react-bootstrap";
-import "./style.css";
-import { useEffect, useState } from "react";
-import { getAllProducts } from "../../../Service/Api";
-import { Link } from "react-router-dom";
-
+import { Card, Button } from 'react-bootstrap'
+import './style.css'
+import { useEffect, useState } from 'react'
+import { getAllProducts } from '../../../Service/Api'
+import { Link } from 'react-router-dom'
 
 export default function CardNovos() {
-
-
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
-    getAllProducts().then(data => setProducts(data.content));
-  }, []);
-
+    getAllProducts().then(data => setProducts(data.content))
+  }, [])
 
   return (
     <div className="group_card container">
       {/* slice limita o número de produtos que serão exibidos */}
-      {products.slice(0, 4).map((product) => (
-        <Card className="box_cardNovos" style={{ width: '18rem' }} key={product.id}>
-          <div className="box_imageVovos">
-            <Card.Img className="image_novos" variant="top" src={product.image} />
+      {products.slice(0, 4).map(product => (
+        <Card
+          className="box_cardNovos"
+          style={{ width: '18rem' }}
+          key={product.id}
+        >
+          <div className="box_imageNovos">
+            <Card.Img
+              className="image_novos"
+              variant="top"
+              src={product.image}
+            />
           </div>
           <Card.Body>
             <Card.Title>{product.title}</Card.Title>
@@ -31,7 +35,9 @@ export default function CardNovos() {
             <Card.Footer>
               <div className="d-grid gap-2 Box_botao_novos">
                 <Link to={`/details/${product.id}`}>
-                  <Button className="link-buy" variant="outline-dark" size="lg">Comprar por R$ {product.price}</Button>
+                  <Button className="link-buy" variant="outline-dark" size="lg">
+                    Comprar por R$ {product.price}
+                  </Button>
                 </Link>
               </div>
             </Card.Footer>
@@ -39,6 +45,5 @@ export default function CardNovos() {
         </Card>
       ))}
     </div>
-
-  );
+  )
 }
