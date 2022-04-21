cd1e2bea0c5549c6a9bdc26e9ae1153106af1b13
PauloRossi <paulogustavorossi@gmail.com>
2022-04-20 23:37:26 -0300

    subindo o footer responsivo

--- src/Pages/Footer/index.js
+++ src/Pages/Footer/index.js
@@ -11,15 +11,15 @@ export default function Footer() {
             <Navbar className="container"  variant="dark" sticky="botton" expand="sm">
                 <div className="container flex-column">                
                     <div className='d-flex justify-content-between w-100 container-contato'>
-                        <div className='d-flex '>
+                        <div className='d-flex box_logo_contato'>
                             <img className="merc_logo" src={merc} alt="Logo Merch Tech" />              
-                            <div className="text-white text-center box_sobre_logo">
+                            <div className="text-white box_sobre_logo">
                                 <p className="Text_sobre_logo">+ 55 11 9 6106-4451</p>
                                 <p className="Text_sobre_logo">contato@merctech.com.br</p>
                             </div>
                         </div> 
 
-                        <div className='d-flex'>
+                        <div className='d-flex box_mail_whats'>
                             <Nav.Link className="link-to" href="https://wa.me/550000000000" target="_blank" id='mygit' >
                                 <div className="box_icon_footer">
                                     <BsWhatsapp className="Icon-color_footer" />
@@ -33,7 +33,7 @@ export default function Footer() {
                         </div>   
 
                     </div>                             
-                    <div className="container text-white text-center">
+                    <div className="container text-white text-center text_foter_info">
                         <p className="Text_sobre_footer">Merc Tech Varejo do Brasil Ltda. | CNPJ 08.888.440/0001-39</p>
                         <p className="Text_sobre_footer">Rua Treze de Maio, 3458 Centro, São Paulo/SP CEP: 14870-160</p>
                         <p className="Text_sobre_footer">Formas de pagamentos aceitas: cartões de crédito (MasterCard, Visa, Elo e American Express) e boleto.</p>
