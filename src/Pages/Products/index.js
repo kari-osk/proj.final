e978183f6cd3567c586af47a0689e8ebc121ebdf
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-04-22 19:11:21 -0300

    colocando paginação

--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,8 +1,9 @@
-import { Card, ListGroupItem, ListGroup } from "react-bootstrap"
+import { Card, ListGroupItem, ListGroup, Pagination } from "react-bootstrap"
 import { useState, useEffect } from "react"
 import "./style.css"
-import {Link, useParams, useNavigate } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom"
 import { formatMoney } from "../Administracao/useUtils"
+
 
 
 export default function Products() {
@@ -10,52 +11,61 @@ export default function Products() {
     const [products, setProducts] = useState([])
     const [categories, setCategories] = useState([])
 
+    const [pageNumber, setPageNumber] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
+
     const params = useParams();
     let navigate = useNavigate()
 
     useEffect(() => {
 
         console.log(params.category)
-        if(params.category === "all"){
+        if (params.category === "all") {
             getProducts();
-        }else{
+        } else {
             getProductsByCategory(params.category)
         }
 
         getCategory();
-    }, [navigate])
+    }, [navigate, pageNumber])
 
     async function getProducts() {
         try {
-            const data = await fetch("http://3.16.56.233:8080/products")
-            const { content } = await data.json()
+            const data = await fetch(
+              `http://3.16.56.233:8080/products?size=10&page=${pageNumber}`
+            )
+            const { content, totalPages, last, first } = await data.json()
+      
             setProducts(content)
-        } catch (error) {
+            setTotalPages(totalPages)
+            // setLastPage(last)
+            // setFirstPage(first)
+          } catch (error) {
             alert('Houve um erro de comunicação com o servidor.', error)
-        }
+          }
     }
 
     async function getCategory() {
         try {
-          const data = await fetch('http://3.16.56.233:8080/categories')
-          const response = await data.json()
-          setCategories(response)
+            const data = await fetch('http://3.16.56.233:8080/categories')
+            const response = await data.json()
+            setCategories(response)
         } catch (error) {
-          alert('Houve um erro de comunicação com o servidor.', error)
+            alert('Houve um erro de comunicação com o servidor.', error)
         }
     }
 
     async function getProductsByCategory(id) {
         try {
             const data = await fetch(`http://localhost:8080/products/category/${id}`)
-            const  content  = await data.json()
+            const content = await data.json()
             setProducts(content)
         } catch (error) {
             alert('Houve um erro de comunicação com o servidor.', error)
         }
     }
-    
-    function selectCategory(category){
+
+    function selectCategory(category) {
         navigate(`../products/${category}`, { replace: true })
     }
 
@@ -68,32 +78,46 @@ export default function Products() {
                     <h2>Categorias</h2>
                     <ul>
                         <li onClick={() => selectCategory("all")}>Todas</li>
-                        {categories.map( category => 
+                        {categories.map(category =>
                             <li onClick={() => selectCategory(category.id)} key={category.id}> {category.name}</li>
                         )}
-    
+
                     </ul>
                 </div>
             </aside>
-
             <div className=" container-products-card">
-            {products.map(product =>
+                <div >
+                    {products.map(product =>
 
-                <Link to={`/details/${product.id}`}>              
-                    <Card className="products-card" key={product.id} >
-                        <div>
-                            <Card.Img className="img-fluid" variant="top" src={product.image} />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>{product.title}</Card.Title>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>{product.category.name}</ListGroupItem>
-                            <ListGroupItem className="list-group-price" >{formatMoney(product.price)}</ListGroupItem>
-                        </ListGroup>
-                    </Card>
-                </Link>  
-            )}
+                        <Link to={`/details/${product.id}`}>
+                            <Card className="products-card" key={product.id} >
+                                <div>
+                                    <Card.Img className="img-fluid" variant="top" src={product.image} />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title>{product.title}</Card.Title>
+                                </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>{product.category.name}</ListGroupItem>
+                                    <ListGroupItem className="list-group-price" >{formatMoney(product.price)}</ListGroupItem>
+                                </ListGroup>
+                            </Card>
+                        </Link>
+                    )}
+                </div>
+
+                    
+                <div className="container-pagination">
+                    <Pagination >
+                        <Pagination.First />
+                        <Pagination.Prev onClick={() => setPageNumber(pageNumber -1)} />
+                        <Pagination.Item active>{pageNumber+1}</Pagination.Item>
+                        <Pagination.Ellipsis />
+                        <Pagination.Item onClick={() => setPageNumber(totalPages-1)} >{totalPages}</Pagination.Item>
+                        <Pagination.Next onClick={() => setPageNumber(pageNumber +1)} />
+                        <Pagination.Last />
+                    </Pagination>
+                </div>
             </div>
         </section>
     );
